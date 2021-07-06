e19d5306c2c32060c1d61869b075b97fcd6e11da
chen hong <vvchennnnn@gmail.com>
2021-07-06 22:05:47 +1000

    booking inital version without error validation

--- src/pages/BookingForm/BookingForm.jsx
+++ src/pages/BookingForm/BookingForm.jsx
@@ -12,9 +12,9 @@ const Title = styled.h2`
     `,
     secondary: css`
       margin: 54px 0px 27px 0px;
-    `
+    `,
   }[props.variant])}
-`
+`;
 
 const SubTitle = styled.p`
   font-family: 'Raleway';
@@ -23,13 +23,13 @@ const SubTitle = styled.p`
   letter-spacing: 0.14px;
   text-align: center;
   margin: 9px 0 40px 0;
-`
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const Input = styled.input`
   border: none;
@@ -46,14 +46,14 @@ const Input = styled.input`
     lg: css`
       width: 360px;
       margin: 0px 0px 21px 0px;
-    `
+    `,
   }[props.variant])}
-`
+`;
 
 const Checkbox = styled.div`
   font-family: 'Raleway';
   text-align: left;
-`
+`;
 
 export default class BookingForm extends React.Component {
   constructor(props) {
@@ -61,17 +61,17 @@ export default class BookingForm extends React.Component {
 
     this.state = {
       data: {
-        selectedDate: "",
-        guestNumber: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-        phoneNumber: "" ,
-        birthDate: "",
+        selectedDate: '',
+        guestNumber: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        birthDate: '',
         ageChecked: false,
         towelChecked: false,
-        acknowledgeChecked: false
-      }
+        acknowledgeChecked: false,
+      },
     };
 
     this.handleDataChange = this.handleDataChange.bind(this);
@@ -79,18 +79,18 @@ export default class BookingForm extends React.Component {
 
   handleDataChange(event) {
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    const name = event.target.name;
+    const { name } = event.target;
 
     this.setState((prevState) => ({
       data: {
         ...prevState.data,
-        [name]: value
-      }
+        [name]: value,
+      },
     }));
   }
 
   render() {
-    const { data } = this.state
+    const { data } = this.state;
 
     return (
       <>
@@ -103,95 +103,102 @@ export default class BookingForm extends React.Component {
           }}
         >
           <div>
-            <Input variant="sm"
+            <Input
+              variant="sm"
               name="selectedDate"
-              placeholder="06/06/2021" 
+              placeholder="06/06/2021"
               type="text"
-              value={data.selectedDate} 
+              value={data.selectedDate}
               disabled
             />
-            <Input variant="sm"
+            <Input
+              variant="sm"
               name="guestNumber"
-              placeholder="Number of guests" 
-              value={data.guestNumber} 
+              placeholder="Number of guests"
+              value={data.guestNumber}
               type="number"
-              onChange={this.handleDataChange} 
+              onChange={this.handleDataChange}
             />
           </div>
           <div>
-            <Input variant="sm"
+            <Input
+              variant="sm"
               name="firstName"
-              placeholder="First Name" 
+              placeholder="First Name"
               type="text"
-              value={data.firstName} 
-              onChange={this.handleDataChange} 
+              value={data.firstName}
+              onChange={this.handleDataChange}
             />
-            <Input variant="sm"
+            <Input
+              variant="sm"
               name="lastName"
-              placeholder="Last Name" 
+              placeholder="Last Name"
               type="text"
-              value={data.lastName} 
-              onChange={this.handleDataChange} 
-            /> 
+              value={data.lastName}
+              onChange={this.handleDataChange}
+            />
           </div>
-          <Input variant="lg"
+          <Input
+            variant="lg"
             name="email"
-            placeholder="Email" 
+            placeholder="Email"
             type="email"
-            value={data.email} 
-            onChange={this.handleDataChange} 
+            value={data.email}
+            onChange={this.handleDataChange}
           />
-          <Input variant="lg"
+          <Input
+            variant="lg"
             name="phoneNumber"
-            placeholder="Phone number" 
+            placeholder="Phone number"
             type="number"
-            value={data.phoneNumber} 
-            onChange={this.handleDataChange} 
+            value={data.phoneNumber}
+            onChange={this.handleDataChange}
           />
-          <Input variant="lg"
+          <Input
+            variant="lg"
             name="birthDate"
-            placeholder= "Date of birth"
+            placeholder="Date of birth"
             type="date"
-            value={data.birthDate} 
-            onChange={this.handleDataChange} 
+            value={data.birthDate}
+            onChange={this.handleDataChange}
           />
           <Title variant="secondary">Please read and select all before the payment</Title>
           <Checkbox>
             <label htmlFor="ageChecked">
-              <input 
+              <input
                 name="ageChecked"
-                type="checkbox" 
+                type="checkbox"
                 id="ageChecked"
                 checked={data.ageChecked}
-                onChange={this.handleDataChange} 
+                onChange={this.handleDataChange}
               />
               All guest in my booking are over 15 years of age
-            </label>     
+            </label>
             <br />
             <label htmlFor="towelChecked">
-              <input 
-                name="towelChecked" 
-                type="checkbox" 
+              <input
+                name="towelChecked"
+                type="checkbox"
                 id="towelChecked"
                 checked={data.towelChecked}
-                onChange={this.handleDataChange} 
+                onChange={this.handleDataChange}
               />
               I will either bring my own large bath towel or purchase one on the day
             </label>
             <br />
             <label htmlFor="acknowledgeChecked">
-              <input 
-                name="acknowledgeChecked" 
-                type="checkbox" 
+              <input
+                name="acknowledgeChecked"
+                type="checkbox"
                 id="acknowledgeChecked"
                 checked={data.acknowledgeChecked}
-                onChange={this.handleDataChange} 
+                onChange={this.handleDataChange}
               />
               I understand this is a booking enquiry and not confirmation of booking
             </label>
           </Checkbox>
         </Form>
       </>
-    )
+    );
   }
 }
