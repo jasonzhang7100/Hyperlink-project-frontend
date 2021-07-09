01e0b51d16883355f7f2e489ba986f8dfae1d630
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-09 22:10:37 +1000

    modify HeaderBar

--- src/components/BookingHeaderBar/BookingHeaderBar.jsx
+++ src/components/BookingHeaderBar/BookingHeaderBar.jsx
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 import LogoPic from '../../assets/owl.png';
 
 const Wrapper = styled.div`
-height: 118px;
-margin: 0 0 85px;
-background-color: #181b50;
+  height: 118px;
+  margin: 0 0 85px;
+  background-color: #181b50;
 `;
 const HeaderBarLogo = styled.div`
-margin:0 auto;
-width:105px;
-height:118px;
-background-image:url(${LogoPic});
-background-repeat:no-repeat;
-background-size:100%;
+  margin: 0 auto;
+  width: 105px;
+  height: 118px;
+  background-image: url(${LogoPic});
+  background-repeat: no-repeat;
+  background-size: 100%;
 `;
 
 const BookingHeaderBar = () => (
