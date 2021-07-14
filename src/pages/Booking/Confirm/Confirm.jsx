429c71f3668316a3b4ab4036d84fa47374d77f30
SUNNY <hotsummer2626@gmail.com>
2021-07-14 12:27:21 +1000

    rebase master

--- src/pages/Booking/Confirm/Confirm.jsx
+++ src/pages/Booking/Confirm/Confirm.jsx
@@ -28,27 +28,24 @@ const Line = styled.div`
   background-color: #c7c7c7;
 `;
 
-const Confirm = ({ paid }) =>
-  paid === true ? (
-    <Container>
-      <FontAwesomeIcon color="#181b50" size="4x" icon={faCheckCircle} />
-      <Title className="title">Booking Confirmed</Title>
-      <ConfirmedMessage>
-        Thank you John, we&apos;re looking forward to see you soon!
-        <br />
-        Confirmation email send to &nbsp;
-        <a href="www.gmail.com">xxx@gmail.com</a>
-      </ConfirmedMessage>
+const Confirm = () => (
+  <Container>
+    <FontAwesomeIcon color="#181b50" size="4x" icon={faCheckCircle} />
+    <Title className="title">Booking Confirmed</Title>
+    <ConfirmedMessage>
+      Thank you John, we&apos;re looking forward to see you soon!
+      <br />
+      Confirmation email send to &nbsp;
+      <a href="www.gmail.com">xxx@gmail.com</a>
+    </ConfirmedMessage>
 
-      <Line />
+    <Line />
 
-      <BookingInfo date="2021-07-06" id="#12345" guestAmount={1} />
+    <BookingInfo date="2021-07-06" id="#12345" guestAmount={1} />
 
-      <Line />
-    </Container>
-  ) : (
-    <div>付款失败</div>
-  );
+    <Line />
+  </Container>
+);
 
 // 加个按钮。如果付款成功，点击按钮发送formDAta给后端
 
