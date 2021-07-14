d97e51be02ca58d87c3a511fc34d5e5314ccaad3
SUNNY <hotsummer2626@gmail.com>
2021-07-14 12:27:21 +1000

    conform page in progress v1

--- src/pages/Booking/Confirm/Confirm.jsx
+++ src/pages/Booking/Confirm/Confirm.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 
-const Confirm = () => <div>this is the confirm page</div>;
+const Container = styled.div`
+  text-align: center;
+  width: 380px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: normal;
+  margin: 24px 0;
+`;
+
+const ConfirmedMessage = styled.div`
+  font-size: 14px;
+  margin-bottom: 30px;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #c7c7c7;
+`;
+
+const Confirm = () => (
+  <Container>
+    <FontAwesomeIcon color="#181b50" size="4x" icon={faCheckCircle} />
+    <Title className="title">Booking Confirmed</Title>
+    <ConfirmedMessage>
+      Thank you John, we&apos;re looking forward to see you soon!
+      <br />
+      Confirmation email send to &nbsp;
+      <a href="www.gmail.com">xxx@gmail.com</a>
+    </ConfirmedMessage>
+
+    <Line />
+
+    <Line />
+  </Container>
+);
 
 export default Confirm;
