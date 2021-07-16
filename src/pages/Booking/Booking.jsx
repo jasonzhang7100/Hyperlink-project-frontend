3a42d7558dc30d1c9d1ed5080b5896a7decaabd0
SUNNY <hotsummer2626@gmail.com>
2021-07-16 18:02:13 +1000

    resolve eslint

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import Form from '../../components/Form';
-import Payment from './Payment';
-import Confirm from './Confirm';
-import ProgressionBar from './ProgressionBar';
+import Form from "../../components/Form";
+import Payment from "./Payment";
+import Confirm from "./Confirm";
+import ProgressionBar from "./ProgressionBar";
 
 const Container = styled.div`
   display: flex;
@@ -59,7 +59,7 @@ class Booking extends React.Component {
     if (step > 1) {
       this.setState({ step: step - 1 });
     } else {
-      history.push('/');
+      history.push("/");
     }
   }
 
@@ -87,11 +87,7 @@ class Booking extends React.Component {
           />
         )}
         {step === 3 && <Confirm paid={paid} formData={formData} />}
-        <GoBackButton onClick={this.handlePreStep}>
-          {'<'}
-          {' '}
-          go back
-        </GoBackButton>
+        <GoBackButton onClick={this.handlePreStep}>{"<"} go back</GoBackButton>
       </Container>
     );
   }
